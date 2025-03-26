4f84180fce75874a6961a317a3520d518b5896a0
letrongnghia1203 <166737665+letrongnghia1203@users.noreply.github.com>
2025-03-27 00:26:25 +0700

    Project.py

--- Project.py
+++ Project.py
@@ -77,7 +77,7 @@ def classify_sentiment(score):
 start_date = datetime(2015, 1, 1).strftime('%Y-%m-%d')
 end_date = datetime.today().strftime('%Y-%m-%d')
 
-stock = Vnstock().stock(symbol='ACB', source='VCI')
+stock = Vnstock().stock(symbol='ACB', source='TCBS')
 
 @st.cache_data
 def get_vnindex_data():
@@ -131,7 +131,7 @@ def create_vnindex_chart(df):
     return fig
 
 def get_stock_data(symbol):
-    stock = Vnstock().stock(symbol=symbol, source='VCI')  
+    stock = Vnstock().stock(symbol=symbol, source='TCBSTCBS')  
     end_date = datetime.today().strftime('%Y-%m-%d')
     start_date = (datetime.today() - timedelta(days=365*5)).strftime('%Y-%m-%d')  
     df = stock.quote.history(symbol=symbol, start=start_date, end=end_date, interval='1D')
